a3b258fdbc94709889e209d23b7241bd962c811f
John Doe <saullainez@hotmail.es>
2018-09-16 18:55:54 -0600

    Actualización de comentarios

--- index.js
+++ index.js
@@ -1,57 +1,76 @@
-var express = require('express');
-var app = express(); 
-var server = require('http').Server(app);
-const Wit = require('node-wit').Wit;
+//Para iniciar un servidor en Express
+var express = require('express'); //Llamamos a la librería Express (Importar)
+var app = express(); //Ejecutamos la librería Express (Instanciar)
+//Es necesario instalar Express por npm, con el comando: npm install express --save (el --save es para que lo escriba en el package.json)
+var server = require('http').Server(app);//Creamos el servidor http y le pasamos la aplicación Express creada (app)
+const Wit = require('node-wit').Wit;//Llamamos a la librería Wit
+
+//Usamos el framework "static" para usar ficheros estáticos (ejemplo imágenes, css, etc.) en nuestro proyecto
 app.use(express.static('public'));
 
-
+//Token de acceso de nuestra aplicación en Wit
 var ACCESS_TOKEN = "ZBWEIXAHRV7QFSNZCZWL2GTY6DME7CLF";
 
-
+//Esta función envía la frase a nuestra aplicación en Wit para obtener la data procesada por el PLN
 function enviarFrase(frase) {
     console.log('Enviando frase: "'+ frase +'" a Wit.AI');
-
-    const cliente = new Wit({accessToken: ACCESS_TOKEN});
-    cliente.message(frase, {})
-    .then((data) => {
-      console.log('Respuesta: ' + JSON.stringify(data));
-      procesarFrase(data);
+//Instanciamos un nuevo cliente de Wit
+    const cliente = new Wit({accessToken: ACCESS_TOKEN}); //Creamos el cliente con el Token de nuestra app
+    cliente.message(frase, {}) //Mediante el método "message" enviamos la frase a Wit, usando la instancia de nuestra app recién creada
+    .then((data) => { //En data obtenemos la respuesta de Wit (o sea la frase procesada por PLN)
+      console.log('Respuesta: ' + JSON.stringify(data)); //Mostramos el JSON "raw" o crudo
+      procesarFrase(data); //Llamamos a la función que nos va a procesar esa respuesta para obtener la intención identificada por el PLN
     })
     .catch(console.error);
   }
- 
+ //Esta función recibe la data de Wit, cruda, y la procesa para sacar la intención identificada por el PLN
 function procesarFrase(data){
     console.log("Procesando la data");
-    console.log('Mensaje recibido: "'+ data._text +'"');
-    var intenciones = data.entities.intent;
-    var maxConf = 0;
-    var mejorIntencion = "";
+    console.log('Mensaje recibido: "'+ data._text +'"');//En data._text viene el texto o la frase que Wit ha procesado
+    var intenciones = data.entities.intent;//En intenciones almacenamos todas las intenciones que Wit ha identificado y almacenado en la entidad "intent"
+    /*Las dos líneas siguientes las escribimos porque Wit muchas veces identifica más de una intención, cada intención
+    tiene un valor de confianza que determina qué tan acertada es esa intención respecto a la frase enviada,
+    entonces necesitamos obtener la intención con mayor grado de confianza porque esta sería la intención que
+    más se acerque a lo que buscamos con el entrenamiento*/
+    var maxConf = 0;//Variable para identificar la máxima confianza de una intención
+    var mejorIntencion = "";//Variable para almacenar la intención que tiene la mayor confianza
+    /*En este ciclo recorremos las intenciones que Wit ha identificado, para poder obtener lo descrito 
+    en la parte de arriba (La intención con mayor grado de confianza)*/
     for (i = 0; i < intenciones.length; i++){
-        var confianza = intenciones[i].confidence;
-        if (confianza > maxConf) {
+        var confianza = intenciones[i].confidence;//Obtenemos el grado de confianza para esa entidad
+        /*Si la confianza en la intención actual es mayor que la confianza máxima encontrada hasta el momento,
+        entonces esa confianza pasa a ser la confianza máxima y la intención que tenga ese grado de confianza,
+        pasa a ser la mejor intención*/
+        if (confianza > maxConf) { 
             maxConf = confianza;
             mejorIntencion = intenciones[i].value;
         }
     }
+    //Debug para identificar que sea la mejor intención, con el grado de confianza más alta
     console.log("Intención identificada: " + mejorIntencion);
+    //Llamamos a la función que va a responder al usuario, según la intención identificada
     responder(mejorIntencion);
 }
 
+//Esta función responde al usuario, según la intención identificada
 function responder(intencion){
+    //Switch que va a responder según la intención
     switch(intencion) {
         case "Saludo":
             console.log("Respuesta del bot: Hola, qué tal");
             break;
     }
 }
+
+//El servidor muestra el index.html cuando se ingresa a la raíz del sitio
 app.get('/', function(req, res){
     res.render('index.html');
 });
 
-
-
-
+//Ejecutamos el servidor
 server.listen(8080, function(){
-    console.log("Servidor corriendo en http://localhost:8080");
+    //console.log("Servidor corriendo en http://localhost:8080");
 });
+/*Frase de prueba, esta función se llamará cuando se ejecute un método post y se enviará la 
+frase que venga en el request*/
 enviarFrase('Hola man');
