d9367708015cdcea8730478cef40716f8a329ef2
John Doe <saullainez@hotmail.es>
2018-10-09 13:22:46 -0600

    Validaciones

--- public/js/postvue.js
+++ public/js/postvue.js
@@ -2,25 +2,35 @@ var app = new Vue({
     el: '#formulario',
     data:{
         visible: false,
+        verror: false,
         frase: '',
         errores: [],
+        error: '',
         intencion: '',
         respuesta: '',
     },
     methods: {
         enviarFrase: function(){
-            var url = '/vue';
-            axios.post(url, {
-                frase: this.frase
-            }).then(response => {
-                this.visible = true;
-                this.frase = '';
-                this.errores = [];
-                intencion = response.data.intencion;
-                this.respuesta = response.data.Respuesta;
-            }).catch(error => {
-                this.errores = error.response.data
-            });
+            if(this.frase){
+                var url = '/vue';
+                axios.post(url, {
+                    frase: this.frase
+                }).then(response => {
+                    this.visible = true;
+                    this.verror = false;
+                    this.frase = '';
+                    this.errores = [];
+                    intencion = response.data.intencion;
+                    this.respuesta = response.data.Respuesta;
+                }).catch(error => {
+                    this.errores = error.response.data
+                });           
+            }else {
+                this.error = "No seas como el maestro, escribí algo para enviar al Bot";    
+                this.visible = false;
+                this.verror = true; 
+            }
+            
         }
     }
 })
